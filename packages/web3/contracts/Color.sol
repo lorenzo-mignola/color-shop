443f1df60768e330d3b7bdbfb8032778a5307108
lorenzo-mignola <lo.mignola@gmail.com>
2022-01-03 16:13:45 +0100

    Add balance & getAllColors

--- packages/web3/contracts/Color.sol
+++ packages/web3/contracts/Color.sol
@@ -2,15 +2,42 @@ pragma solidity 0.8.11;
 
 contract Color {
     mapping(string => address) public colorToOwner;
+    string[] private colorsBought;
+
+    struct UserColor {
+        address user;
+        string color;
+    }
 
     event ColorBought(string indexed color, address _buyer);
 
     function buyColor(string calldata _color) external payable {
         require(colorAvailable(_color), "Color already bought");
         colorToOwner[_color] = msg.sender;
+        colorsBought.push(_color);
     }
 
     function colorAvailable(string memory _color) public view returns (bool) {
         return colorToOwner[_color] == address(0);
+    }
+
+    function totalSpend() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getAllColors() external view returns (UserColor[] memory) {
+        UserColor[] memory colors;
+
+        uint256 arrayLength = 0;
+        for (uint256 i = 0; i < colorsBought.length; i++) {
+            string memory color = colorsBought[i];
+            UserColor memory userColor = UserColor({
+                user: colorToOwner[colorsBought[i]],
+                color: color
+            });
+            colors[arrayLength] = userColor;
+            arrayLength++;
+        }
+        return colors;
     }
 }
